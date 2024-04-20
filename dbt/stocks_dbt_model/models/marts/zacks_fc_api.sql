520d29a78195a73dfa1e845d60cfbe938e38296c
samlawson1 <1samlawson1@gmail.com>
2024-04-20 17:42:16 -0400

    added comments

--- dbt/stocks_dbt_model/models/marts/zacks_fc_api.sql
+++ dbt/stocks_dbt_model/models/marts/zacks_fc_api.sql
@@ -10,10 +10,10 @@ SELECT
 {{ dbt_utils.generate_surrogate_key(['m_ticker', 'per_end_date'])
   }} AS report_id
 ,
-
+--SELECT the rest of the columns
 *
 ,
-
+--Generate an insert_date column with the date the script is executed and data inserted into the table
 CURRENT_DATE AS insert_date
 FROM
 {{source('landing', 'zacks_fc_api')}}
