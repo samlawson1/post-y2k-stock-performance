55ed367998134100afadac2e52366f81a920ae72
samlawson1 <1samlawson1@gmail.com>
2024-04-21 11:37:28 -0400

    airflow dag

--- airflow/dags/stock_elt_dag.py
+++ airflow/dags/stock_elt_dag.py
@@ -1,14 +1,47 @@
+from airflow import DAG 
 from datetime import datetime
-from airflow.decorators import task, dag
 from airflow.providers.docker.operators.docker import DockerOperator
 
-@dag(start_date=datetime(2024, 5, 1, 18,), schedule_interval="@daily", catchup=False)
-
-def daily_etl_dag():
+with DAG('stock_elt',
+         start_date=datetime(2024, 4, 30),
+         #run dag every weekday at 6:30 PM
+         schedule_interval='30 18 * * 1-5',
+         catchup=False) as dag:
     
-    verify_s_and_p_500_task = DockerOperator(
-        task_id = 'verify_S&P_500',
+    task_1 = DockerOperator(
+        task_id = 'verify_s_and_p_500',
+        docker_url='unix://var/run/docker.sock',
+        api_version='auto',
+        auto_remove=True,
         image='s_and_p_500_verification:v1',
-        docker_url='tcp://docker-proxy:2375',
-        network_mode='bridge'
+        container_name='s_and_p_verification_container'
     )
+
+    task_2A = DockerOperator(
+        task_id = 'get_daily_stock_prices',
+        docker_url='unix://var/run/docker.sock',
+        api_version='auto',
+        auto_remove=True,
+        image='wiki-prices:v2',
+        container_name='wiki_prices_container'
+    )
+
+    task_2B = DockerOperator(
+        task_id = 'get_zacks_fc_data',
+        docker_url='unix://var/run/docker.sock',
+        api_version='auto',
+        auto_remove=True,
+        image='zacks-fc:v2',
+        container_name='zacks_fc_api_container'
+    )
+
+    task_3 = DockerOperator(
+        task_id = 'execute_dbt_model',
+        docker_url='unix://var/run/docker.sock',
+        api_version='auto',
+        auto_remove=True,
+        image='dbt_model:v1',
+        container_name='dbt_stock_model_container'
+    )
+
+task_1 >> [task_2A, task_2B] >> task_3
